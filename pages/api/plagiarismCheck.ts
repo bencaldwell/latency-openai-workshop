2614a1a4435e6ed134e7c011ffcc0da371de3006
Ben Caldwell <ben.caldwell@cirruswest.com.au>
2023-06-01 17:02:31 +0800

    COpy past to the wrong function

--- pages/api/plagiarismCheck.ts
+++ pages/api/plagiarismCheck.ts
@@ -11,25 +11,8 @@ type ScanRequest = {
 };
 
 export default async function handler(req: NextRequest) {
-    const body = await req.json();
-    const scanId = body.scannedDocument.scanId;
-    const { resultId, matchedWords } = getHighestSourceResult(body);
-    if (matchedWords != 0) {
-      const copyLeaks = new CopyLeaksWrapper();
-      await copyLeaks.getDetailedResults(scanId, resultId);
-    }
-    try {
-      await fetch(
-        `${process.env.NEXT_PUBLIC_FIREBASE_REALTIME_DATABASE_URL}/scans/${scanId}.json`,
-        {
-          method: "PUT",
-          body: JSON.stringify({ matchedWords: matchedWords }),
-        }
-      );
-    } catch (e) {
-      console.error("Error writing to Firebase Database", e);
-      throw e;
-    }
-  
-    return NextResponse.json({ message: "Scan complete" });
-  }
+  const copyLeaks = new CopyLeaksWrapper();
+  const body = (await req.json()) as ScanRequest;
+  const scanId = await copyLeaks.scan(body.text);
+  return NextResponse.json({ scanId });
+}
